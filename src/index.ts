8b009355230622546caba0ada60b0490c8c2009c
Joseph Fortman <jdbf89@gmail.com>
2023-08-31 20:41:39 -0500

    moved barb.ts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Message } from 'discord.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', (c) => {
@@ -9,5 +9,11 @@ client.once('ready', (c) => {
 const token = process.env.DISCORD_TOKEN;
 console.log(`token length: ${token.length}`);
 client.login(token);
+
+client.on('message', (message: Message) => {
+  if (message.content.match(/\bba+rb\b/gi)) {
+    message.channel.send('BARB')
+  }
+});
 
 export default client;
