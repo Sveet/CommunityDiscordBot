3bab7d5baa2fa4d377ff0ca0258d6e19d2e29787
Joseph Fortman <jdbf89@gmail.com>
2023-09-02 12:06:43 -0500

    add async/await for message send

--- src/barb.ts
+++ src/barb.ts
@@ -1,8 +1,8 @@
 import { Message } from 'discord.js';
 import client from './index';
-client.on('message', (message: Message) => {
+client.on('message', async (message: Message) => {
   console.log(`message received in #${message.channelId}: ${message.author.displayName} "${message.content}"`);
   if (message.content.match(/\bba+rb\b/gi)) {
-    message.channel.send('BARB')
+    await message.channel.send('BARB')
   }
 });
