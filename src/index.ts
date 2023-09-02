108dbf3099b5aee32eb3e8e1c140b5b70469893a
Joseph Fortman <jdbf89@gmail.com>
2023-09-02 12:36:36 -0500

    use message create event; set bot username

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import messageEvents from './messages'
-import { Client, GatewayIntentBits, Message } from 'discord.js';
+import { Client, Events, GatewayIntentBits, Message } from 'discord.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', (c) => {
   console.log(`Discord Client Ready. ${c.application.name}`);
+
+  c.user.setUsername('-_-');
 });
 
-client.on('message', async (message: Message)=>{
+client.on(Events.MessageCreate, async (message: Message)=>{
   if(message.author.bot) return;
   console.log(`message received in #${message.channelId}: ${message.author.displayName} "${message.content}"`);
   
