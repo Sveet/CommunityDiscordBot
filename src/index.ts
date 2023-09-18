71c8c57550309b6daa7031d812ea8a17b9df00e1
Joseph Fortman <jdbf89@gmail.com>
2023-09-17 22:51:52 -0500

    clean up logging; 118 emoji nick sometimes ;)

--- src/index.ts
+++ src/index.ts
@@ -16,22 +16,9 @@ client.once('ready', (c) => {
 });
 
 client.on(Events.MessageCreate, async (message: Message) => {
-  if (message.author.bot) return;
-  console.log(
-    `Bot has permissions for channel: ${
-      message.channel.type == ChannelType.GuildText
-        ? message.channel.permissionsFor(client.user).has('ViewChannel')
-        : 'idk'
-    }`,
-  );
-  console.log(
-    `Bot has permissions for history: ${
-      message.channel.type == ChannelType.GuildText
-        ? message.channel.permissionsFor(client.user).has('ReadMessageHistory')
-        : 'idk'
-    }`,
-  );
-  console.log(JSON.stringify(message));
+  if (message.author.bot || message.channel.isDMBased()) return;
+
+  console.log(`[${new Date().toISOString()}] (${message.channel?.name}) ${message.author.displayName}: ${message.content}`)
 
   for (const me of messageEvents) {
     if (me.match(message)) {
--- src/messages.ts
+++ src/messages.ts
@@ -9,6 +9,15 @@ const messageEvents: MessageEvent[] = [
       await message.channel.send('BARB');
     },
   },
+  {
+    match: (message) => {
+      return message.author.id == '389486278640664587'  // @nicodegallo
+        && ((Math.random() * 118) % 69 == 23);  // once in a blue moon
+    },
+    action: async (message) => {
+      await message.react('596373668028743693');  // :118:
+    },
+  },
 ];
 
 export type MessageEvent = {
